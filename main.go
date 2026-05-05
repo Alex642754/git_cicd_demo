6f88d86f8569611213fbd1b00d2d51d936326cc1
Yang Shuo <1914592897@qq.com>
2026-05-05 09:21:06 +0800

    test change

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "pong")
+		fmt.Fprintf(w, "pong from cicd")
 	})
 	//没有注释
 	http.ListenAndServe(":8080", nil)
